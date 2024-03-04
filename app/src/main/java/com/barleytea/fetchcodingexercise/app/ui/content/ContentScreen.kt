cf7bc60b41e466dd5c8b8e374085136b466adc39
krohyunjae <yroh3@wisc.edu>
2024-03-04 10:30:07 -0600

    added filter chips

--- app/src/main/java/com/barleytea/fetchcodingexercise/app/ui/content/ContentScreen.kt
+++ app/src/main/java/com/barleytea/fetchcodingexercise/app/ui/content/ContentScreen.kt
@@ -2,7 +2,9 @@ package com.barleytea.fetchcodingexercise.app.ui.content
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,81 +13,55 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Card
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.barleytea.fetchcodingexercise.R
-import com.barleytea.fetchcodingexercise.data.fetch.ExerciseApi.*
+import com.barleytea.fetchcodingexercise.data.fetch.ExerciseApi.Data
 import com.barleytea.fetchcodingexercise.model.ExerciseModel
 import com.barleytea.fetchcodingexercise.model.State
 import com.barleytea.fetchcodingexercise.ui.composables.ErrorScreen
 import com.barleytea.fetchcodingexercise.ui.composables.LoadingIndicator
 import com.barleytea.fetchcodingexercise.ui.composables.TextItem
+import com.barleytea.fetchcodingexercise.ui.theme.FetchCodingExerciseTheme
 import com.barleytea.fetchcodingexercise.ui.theme.basePadding
 import com.barleytea.fetchcodingexercise.utils.require
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContentScreen(
-    homeScreenUiState: ExerciseModel,
+    uiState: ExerciseModel,
     contentPadding: PaddingValues = PaddingValues(0.dp),
+    selectList: (Int) -> Unit,
     refreshAction: () -> Unit
 ) {
-    when(homeScreenUiState.state) {
+    when(uiState.state) {
         State.SUCCESS -> {
-            val list = homeScreenUiState.items
-            LazyColumn(
-                modifier = Modifier,
-                contentPadding = contentPadding
-            ) {
-                stickyHeader {
-                    Surface(
-                        modifier = Modifier
-                            .fillParentMaxWidth()
-                            .padding(horizontal = basePadding)
-                            .heightIn(min = basePadding * 2f)
-                    ) {
-                        Row(
-                            modifier = Modifier.fillMaxSize(),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            TextItem(
-                                modifier = Modifier.weight(1f),
-                                text = stringResource(R.string.list_id)
-                            )
-
-                            TextItem(
-                                modifier = Modifier.weight(1f),
-                                text = stringResource(R.string.name)
-                            )
-                            TextItem(
-                                modifier = Modifier.weight(1f),
-                                text = stringResource(R.string.id)
-                            )
-                        }
-                    }
-
-                }
-                itemsIndexed(list) { idx, data ->
-                    data.name?.let {
-                        ItemCard(data = data)
-                        Spacer(modifier = Modifier.padding(basePadding / 2))
-                        if (idx != homeScreenUiState.items.lastIndex) {
-
-                        }
-                    }
-                }
-            }
+            SuccessScreen(
+                uiState = uiState,
+                contentPadding = contentPadding,
+                selectList = selectList
+            )
         }
 
         State.LOADING -> {
@@ -96,17 +72,114 @@ fun ContentScreen(
             }
         }
         else -> {
-            val isInternetError = homeScreenUiState.state == State.NO_INTERNET
+            val isInternetError = uiState.state == State.NO_INTERNET
             ErrorScreen(
                 modifier = Modifier.fillMaxSize(),
                 icon = if(isInternetError) R.drawable.no_internet else null,
-                errorMessage = if(isInternetError) stringResource(id = R.string.error_no_internet)
-                                    else stringResource(id = R.string.error_unexpected),
+                errorMessage = if(isInternetError) stringResource(R.string.error_no_internet)
+                                    else stringResource(R.string.error_unexpected),
                 refreshAction = refreshAction
             )
         }
-
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SuccessScreen(
+    uiState: ExerciseModel,
+    contentPadding: PaddingValues,
+    selectList: (Int) -> Unit
+) {
+    var selected by remember { mutableIntStateOf(-1) }
+    val scrollState = rememberScrollState()
+    LazyColumn(
+        modifier = Modifier,
+        contentPadding = contentPadding
+    ) {
+        stickyHeader {
+            Surface(
+                modifier = Modifier
+                    .fillParentMaxWidth()
+                    .heightIn(min = basePadding * 2f)
+                    .wrapContentHeight()
+            ) {
+                Column {
+                    Row(
+                        modifier = Modifier
+                            .horizontalScroll(scrollState)
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .padding(bottom = basePadding / 2)
+                    ) {
+                        uiState.listIds.forEach { listId ->
+                            FilterChip(
+                                modifier = Modifier
+                                    .padding(horizontal = basePadding),
+                                selected = listId == selected,
+                                onClick = {
+                                    if (selected == listId) {
+                                        selected = -1
+                                        selectList(-1)
+                                    } else {
+                                        selected = listId
+                                        selectList(listId)
+                                    }
+                                },
+                                label = {
+                                    Text(
+                                        text = stringResource(R.string.list_num, listId)
+                                    )
+                                },
+                                leadingIcon = if (listId == selected) {
+                                    {
+                                        Icon(
+                                            imageVector = Icons.Filled.Done,
+                                            contentDescription = "Done icon",
+                                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+                                        )
+                                    }
+                                } else {
+                                    null
+                                }
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = basePadding),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        TextItem(
+                            modifier = Modifier.weight(1f),
+                            text = stringResource(R.string.list_id)
+                        )
+
+                        TextItem(
+                            modifier = Modifier.weight(1f),
+                            text = stringResource(R.string.name)
+                        )
+                        TextItem(
+                            modifier = Modifier.weight(1f),
+                            text = stringResource(R.string.id)
+                        )
+                    }
+                }
+            }
+
+        }
+        itemsIndexed(uiState.selectedItems) { idx, data ->
+            data.name?.let {
+                ItemCard(data = data)
+                Spacer(modifier = Modifier.padding(basePadding / 2))
+                if (idx != uiState.items.lastIndex) {
+
+                }
+            }
+        }
+    }
+
 }
 
 
@@ -136,5 +209,27 @@ fun ItemCard(data: Data) {
                 text = data.id
             )
         }
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+fun ContentPreview() {
+    FetchCodingExerciseTheme {
+        ContentScreen(
+            uiState = ExerciseModel(
+                items = listOf(
+                    Data("0", 1, "Item 0")
+                ),
+                selectedItems = listOf(
+                    Data("0", 1, "Item 0")
+                ),
+                listIds = listOf(1),
+                state = State.SUCCESS
+            ),
+            selectList = {}
+        ){}
     }
 }
--- app/src/main/java/com/barleytea/fetchcodingexercise/app/ui/home/HomeScreen.kt
+++ app/src/main/java/com/barleytea/fetchcodingexercise/app/ui/home/HomeScreen.kt
@@ -1,15 +1,11 @@
 package com.barleytea.fetchcodingexercise.app.ui.home
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -20,18 +16,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.barleytea.fetchcodingexercise.R
 import com.barleytea.fetchcodingexercise.app.MainActivityViewModel
 import com.barleytea.fetchcodingexercise.app.ui.content.ContentScreen
-import com.barleytea.fetchcodingexercise.ui.theme.basePadding
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = { TopAppBar(scrollBehavior = scrollBehavior) }
@@ -39,12 +33,13 @@ fun HomeScreen() {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            val mainActivityViewModel: MainActivityViewModel = viewModel()
-            val uiState by mainActivityViewModel.uiState.collectAsStateWithLifecycle()
+            val viewModel: MainActivityViewModel = viewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
             ContentScreen(
-                homeScreenUiState = uiState,
+                uiState = uiState,
                 contentPadding = it,
-                refreshAction = mainActivityViewModel::reload
+                selectList = viewModel::selectList,
+                refreshAction = viewModel::loadData
             )
         }
     }
